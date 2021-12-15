e0643384992e22720a1f0b26d28594bfd453447d
Alex <alexander.stcstudent@gmail.com>
2021-12-15 23:28:28 +0300

    added a new branch "home_assignment"

--- src/test/java/HomeAssignment.java
+++ src/test/java/HomeAssignment.java
@@ -4,7 +4,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class BasicTest {
+public class HomeAssignment {
     @Test
     void selenideSearchTest() {
         open("https://demoqa.com/automation-practice-form");
