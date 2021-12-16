c0fecc319b22b0928f2210b9a46452c4a4eb4b1a
Alex <alexander.stcstudent@gmail.com>
2021-12-16 15:06:56 +0300

    added tests to the .java file, added new directories, added new files

--- src/test/java/HomeAssignment.java
+++ src/test/java/HomeAssignment.java
@@ -1,88 +1,63 @@
-/*
-
-Помни!
-
-Хороший код - это код, в котором много комментариев от автора,
-написанные с целью, чтобы любой, кто открыл его, смог понять,
-что в нём происходит, и что нужно делать в тот или иной момент.
-
-*/
-
-// Импортируемые плагины, необходимые для осуществления тех или иных операций
-
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Browsers.OPERA;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-// Наименование class(-а) совпадает с наименованием файла с расширением .java
-
 public class HomeAssignment {
 
-    /*
-
-    Перед каждым выполнением тестирования - будут задействованы следующие конфигурации
-
-    */
-
     @BeforeEach
-
-    // Запустить браузер со следующими параметрами
-
     void browserOpeningConfiguration() {
 
-        // Запускать в браузере "opera"
-        Configuration.browser = "opera";
+        Configuration.browser = OPERA;
+        Configuration.fastSetValue = false;
+        Configuration.browserSize = "1920x1080";
 
-        // Очищать браузер от 'куки'
-//        clearBrowserCookies();
+        clearBrowserCookies();
 
-        // Перед выполнением - открывать заданную страницу
         open("https://demoqa.com/automation-practice-form");
     }
 
     @Test
-
-    /*
-
-    Во время выполнения тестирования полей - будут задействованы таймауты (в миллисекундах),
-    с целью убедиться, что каждое вызванное поле было заполнено без проишествий
-
-    */
-
-    // Вызывается метод, цель которого заполнять искомые поля, названный как 'selenideStudentRegistrationPracticeFormTest'
-    // Наименование метода задано в произвольном порядке
-    // Можно придумать своё название
-
     void selenideStudentRegistrationPracticeFormTest() {
 
-        /* В появившейся форме заполняем поля */
+        $("#firstName").setValue("Alexander");
+        $("[id=\"lastName\"]").setValue("Barashkov");
+        $("[id='userEmail']").setValue("abarashkov@email.ru");
+        $("[for='gender-radio-1']").click();
+        $("[placeholder='Mobile Number']").setValue("8999888447");
+        $("[id='dateOfBirthInput']").click();
+        $(byText("November")).click();
+        $(byText("1992")).click();
+        $("[class*='026']").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $(byText("Sports")).click();
+        File file = new File("src/test/resources/readme.txt");
+        $("input[id='uploadPicture']").uploadFile(file);
+        $("#currentAddress").setValue("Address");
+        $("[id='react-select-3-input']").setValue("Rajasthan").pressEnter();
+        $("[id='react-select-4-input']").setValue("Jaiselmer").pressEnter();
+        $("[id='submit']").click();
 
-        // Name
+        selenideStudentRegistrationPracticeFormTestCheck();
+    }
 
-        // FirstName
-        $("#firstName").setValue("Alexander"); // знак '#' используется с целью сокращения
+    void selenideStudentRegistrationPracticeFormTestCheck() {
 
-        sleep(3000); // Ожидание 3000 миллисекунд
 
-        // LastName
-        $("#lastName").setValue("Barashkov");
-
-        sleep(3000);
-
-        // Email
-        $("#userEmail").setValue("abarashkov@email.ru");
-
-        sleep(3000);
-
-        // Gender
-        $(byText("Male")).click();
-
-        sleep(3000);
-
-        // .pressEnter()
 
     }
+
+    @AfterEach
+    void after(){
+        closeWebDriver();
+    }
+
 }
