6cc4a516776e80bf3be71c21cfc62b5bf5cd4da0
Alex <alexander.stcstudent@gmail.com>
2021-12-22 23:39:01 +0300

    added ordered tests to the .java file, refactored .java file

--- src/test/java/DemoqaAutomationPracticeFormLearning.java
+++ src/test/java/DemoqaAutomationPracticeFormLearning.java
@@ -1,34 +1,35 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 
 import static com.codeborne.selenide.Browsers.CHROME;
-import static com.codeborne.selenide.Browsers.OPERA;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class HomeAssignment {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
-    @BeforeEach
-    void browserOpeningConfiguration() {
+public class DemoqaAutomationPracticeFormLearning {
 
-        Configuration.browser = OPERA;
+    @BeforeAll
+    static void browserOpeningConfiguration() {
+
+        Configuration.browser = CHROME;
         Configuration.fastSetValue = false;
         Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
 
         clearBrowserCookies();
 
-        open("https://demoqa.com/automation-practice-form");
     }
 
     @Test
-    void selenideStudentRegistrationPracticeFormTest() {
+    @Order(1)
+    void selenideStudentRegistrationPracticeFormFilling() {
+
+        open("/automation-practice-form");
 
         $("#firstName").setValue("Alexander");
         $("[id=\"lastName\"]").setValue("Barashkov");
@@ -49,22 +50,94 @@ public class HomeAssignment {
         $("[id='react-select-4-input']").setValue("Jaiselmer").pressEnter();
         $("[id='submit']").click();
 
+    }
+
+    @Test
+    @Order(2)
+    void selenideStudentFilledFormAppearanceChecking() {
+
         $(byText("Thanks for submitting the form")).should(appear);
+
+    }
+
+    @Test
+    @Order(3)
+    void selenideStudentFilledFormVisibilityChecking() {
+
         $(byText("Thanks for submitting the form")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(4)
+    void selenideStudentFilledFormEmailLineVisibilityChecking() {
+
         $(byText("abarashkov@email.ru")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(5)
+    void selenideStudentFilledFormGenderLineVisibilityChecking() {
+
         $(byText("Male")).shouldBe(visible);
+
+        }
+
+    @Test
+    @Order(6)
+    void selenideStudentFilledFormMobilePhoneLineVisibilityChecking() {
+
         $(byText("8999888447")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(7)
+    void selenideStudentFilledFormDateOfBirthLineVisibilityChecking() {
+
         $(byText("26 November,1992")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(8)
+    void selenideStudentFilledFormSubjectsLineVisibilityChecking() {
+
         $(byText("Computer Science, English")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(9)
+    void selenideStudentFilledFormHobbiesLineVisibilityChecking() {
+
         $(byText("Sports")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(10)
+    void selenideStudentFilledFormUploadedFileLineVisibilityChecking() {
+
         $(byText("readme.txt")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(11)
+    void selenideStudentFilledFormAddressLineVisibilityChecking() {
+
         $(byText("My Address")).shouldBe(visible);
+
+    }
+
+    @Test
+    @Order(12)
+    void selenideStudentFilledFormStateAndCityLineVisibilityChecking() {
+
         $(byText("Rajasthan Jaiselmer")).shouldBe(visible);
 
     }
 
-    @AfterEach
-    void after(){
-        closeWebDriver();
-    }
 }
