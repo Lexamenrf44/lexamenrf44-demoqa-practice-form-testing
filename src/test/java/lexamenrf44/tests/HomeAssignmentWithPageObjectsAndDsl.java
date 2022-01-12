8c32d97494b8559e2350859ffd68db7743eed44f
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-01-12 22:42:31 +0300

    editing "page_objects_home_assignment" branch, editing HomeAssignmentWithPageObjectsAndDsl.java, deleted .java class "TestBase" in testbase directory

--- src/test/java/lexamenrf44/tests/HomeAssignmentWithPageObjectsAndDsl.java
+++ src/test/java/lexamenrf44/tests/HomeAssignmentWithPageObjectsAndDsl.java
@@ -1,9 +1,13 @@
 package lexamenrf44.tests;
 
+import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import lexamenrf44.pages.RegistrationForm;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static lexamenrf44.constants.ConstantsForRegForm.*;
 
 public class HomeAssignmentWithPageObjectsAndDsl {
@@ -16,6 +20,17 @@ public class HomeAssignmentWithPageObjectsAndDsl {
     String phone = faker.number().digits(10);
 
     RegistrationForm registrationForm = new RegistrationForm();
+
+    @BeforeEach
+    void browserOpeningConfiguration() {
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = CHROME;
+        Configuration.fastSetValue = false;
+        Configuration.holdBrowserOpen = false;
+
+        clearBrowserCookies();
+    }
 
     @Test
     void studentRegistrationPracticeFormWithPageObjectsTest() {
