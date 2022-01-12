77bcb293c101f5a85188a14889283b9cdc5accc4
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-01-12 22:38:14 +0300

    editing "page_objects_home_assignment" branch, editing HomeAssignmentWithPageObjectsAndDsl.java, added new .java class "TestBase"

--- src/test/java/lexamenrf44/testbase/TestBase.java
+++ src/test/java/lexamenrf44/testbase/TestBase.java
@@ -1,15 +1,12 @@
 package lexamenrf44.testbase;
 
 import com.codeborne.selenide.Configuration;
-import lexamenrf44.pages.RegistrationForm;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 
 public class TestBase {
-
-    RegistrationForm registrationForm = new RegistrationForm();
 
     @BeforeEach
     void browserOpeningConfiguration() {
