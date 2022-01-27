a8e0f302f92da85d37957869ca665611cabd49a6
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-01-27 20:28:05 +0300

    edited BrowserSetup.java, added Selenide listener

--- src/test/java/lexamenrf44/configuration/BrowserSetup.java
+++ src/test/java/lexamenrf44/configuration/BrowserSetup.java
@@ -1,6 +1,8 @@
 package lexamenrf44.configuration;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -10,15 +12,17 @@ public class BrowserSetup {
 
     @BeforeAll
     static void browserOpeningConfiguration() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
 
 
     }
